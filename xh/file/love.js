c1cc9b85720924424133226e88c0c8a8fe9b9dbf
JunshenGB-web <56272579+JunshenGB-web@users.noreply.github.com>
2019-10-09 03:26:56 +0800

    Update love.js

--- xh/file/love.js
+++ xh/file/love.js
@@ -59,8 +59,7 @@
         var points = [], x, y, t;
         for (var i = 10; i < 30; i += 0.2) {
             t = i / Math.PI;
-            x = 16 * Math.pow(Math.sin(t), 3);
-            y = 13 * Math.cos(t) - 5 * Math.cos(2 * t) - 2 * Math.cos(3 * t) - Math.cos(4 * t);
+
             points.push(new Point(x, y));
         }
         this.points = points;
